e2dd2def056712c8c09c11c547447e94027e45ab
Haruke <87328375+xHaruke@users.noreply.github.com>
2022-08-19 10:52:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   userID: "", // user id of the person you want to stalk
-  channelID: "", // channel in which the bot will send the messages.. Only the selfbot should be able to send message in the channel.. The bot will crash if any other message is sent in that channel
+  channelID: "", // channel in which the bot will send the messages..
   serverID: "", // a common server between the self bot and the user
   token: "" || process.env.token, // selfbot's token
 
